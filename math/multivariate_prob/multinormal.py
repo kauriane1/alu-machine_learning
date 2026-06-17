ae9f29941a6efc1fb47ad98a68b1a461aeff14d4
kauriane1 <a.kalisa@alustudent.com>
2026-06-17 06:09:56 -0700

    Multivariate probability

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Multivariate Normal Distribution"""
+"""Multivariate Normal distribution"""
 
 
 import numpy as np
@@ -9,11 +9,7 @@ class MultiNormal:
     """Represents a Multivariate Normal distribution"""
 
     def __init__(self, data):
-        """
-        Class constructor
-
-        data is a numpy.ndarray of shape (d, n)
-        """
+        """Class constructor"""
 
         if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
@@ -30,9 +26,7 @@ class MultiNormal:
         self.cov = np.matmul(X, X.T) / (n - 1)
 
     def pdf(self, x):
-        """
-        Calculates the PDF at a data point
-        """
+        """Calculates the PDF at a data point"""
 
         if not isinstance(x, np.ndarray):
             raise TypeError("x must be a numpy.ndarray")
@@ -40,23 +34,27 @@ class MultiNormal:
         d = self.mean.shape[0]
 
         if x.shape != (d, 1):
-            raise ValueError("x must have the shape ({}, 1)".format(d))
+            raise ValueError(
+                "x must have the shape ({}, 1)".format(d)
+            )
+
+        diff = x - self.mean
 
         det = np.linalg.det(self.cov)
 
         inv = np.linalg.inv(self.cov)
 
-        diff = x - self.mean
-
-        exponent = -0.5 * np.matmul(
-            np.matmul(diff.T, inv),
-            diff
-        )
-
-        denominator = np.sqrt(
+        coeff = 1 / np.sqrt(
             ((2 * np.pi) ** d) * det
         )
 
-        pdf = np.exp(exponent) / denominator
+        exponent = np.exp(
+            -0.5 * np.matmul(
+                np.matmul(diff.T, inv),
+                diff
+            )
+        )
 
-        return float(pdf)
+        pdf = coeff * exponent
+
+        return max(pdf[0][0], 1e-300)
