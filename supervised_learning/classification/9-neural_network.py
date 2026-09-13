8da668212b4fed38279cb52f2afce5659d6eb27b
Kalisa Auriane <a.kalisa@alustudent.com>
2026-09-14 01:25:09 +0200

    Refactor docstrings in NeuralNetwork class
    Updated docstrings for clarity and consistency.

--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -1,58 +1,66 @@
 #!/usr/bin/env python3
+""" Neural Network
+"""
 
 import numpy as np
 
 
 class NeuralNetwork:
-    """Defines a neural network with one hidden layer"""
+    """ Class that defines a neural network with one hidden layer performing
+        binary classification.
+    """
 
     def __init__(self, nx, nodes):
-        """Initialize the neural network"""
+        """ Instantiation function
 
+        Args:
+            nx (int): size of the input layer
+            nodes (_type_): _description_
+        """
         if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
+            raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
+            raise ValueError('nx must be a positive integer')
 
         if not isinstance(nodes, int):
-            raise TypeError("nodes must be an integer")
+            raise TypeError('nodes must be an integer')
         if nodes < 1:
-            raise ValueError("nodes must be a positive integer")
+            raise ValueError('nodes must be a positive integer')
 
         self.__W1 = np.random.randn(nodes, nx)
         self.__b1 = np.zeros((nodes, 1))
         self.__A1 = 0
-
         self.__W2 = np.random.randn(1, nodes)
         self.__b2 = 0
         self.__A2 = 0
 
+    # getter functions
     @property
     def W1(self):
-        """Getter for W1"""
+        """Return weights vector for hidden layer"""
         return self.__W1
 
     @property
     def b1(self):
-        """Getter for b1"""
+        """Return bias for hidden layer"""
         return self.__b1
 
     @property
     def A1(self):
-        """Getter for A1"""
+        """Return activated output for hidden layer"""
         return self.__A1
 
     @property
     def W2(self):
-        """Getter for W2"""
+        """Return weights vector for output neuron"""
         return self.__W2
 
     @property
     def b2(self):
-        """Getter for b2"""
+        """Return bias for the output neuron"""
         return self.__b2
 
     @property
     def A2(self):
-        """Getter for A2"""
+        """Return activated output for output neuron"""
         return self.__A2
