7caee8d25583a1880168f8e498e49eadc0b4d0bd
Kalisa Auriane <a.kalisa@alustudent.com>
2026-09-14 00:43:14 +0200

    Refactor NeuralNetwork class and improve docstrings
    Updated docstrings for clarity and improved formatting. Refactored forward propagation calculations for readability.

--- supervised_learning/classification/10-neural_network.py
+++ supervised_learning/classification/10-neural_network.py
@@ -1,69 +1,80 @@
 #!/usr/bin/env python3
+""" Neural Network
+"""
 
 import numpy as np
 
 
 class NeuralNetwork:
-    """Defines a neural network with one hidden layer"""
+    """ Class that defines a neural network with one hidden layer performing
+        binary classification.
+    """
 
     def __init__(self, nx, nodes):
-        """Initialize the neural network"""
+        """ Instantiation function
 
+        Args:
+            nx (int): size of the input layer
+            nodes (_type_): _description_
+        """
         if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
+            raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
+            raise ValueError('nx must be a positive integer')
 
         if not isinstance(nodes, int):
-            raise TypeError("nodes must be an integer")
+            raise TypeError('nodes must be an integer')
         if nodes < 1:
-            raise ValueError("nodes must be a positive integer")
+            raise ValueError('nodes must be a positive integer')
 
         self.__W1 = np.random.randn(nodes, nx)
         self.__b1 = np.zeros((nodes, 1))
         self.__A1 = 0
-
         self.__W2 = np.random.randn(1, nodes)
         self.__b2 = 0
         self.__A2 = 0
 
+    # getter functions
     @property
     def W1(self):
-        """Getter for W1"""
+        """Return weights vector for hidden layer"""
         return self.__W1
 
     @property
     def b1(self):
-        """Getter for b1"""
+        """Return bias for hidden layer"""
         return self.__b1
 
     @property
     def A1(self):
-        """Getter for A1"""
+        """Return activated output for hidden layer"""
         return self.__A1
 
     @property
     def W2(self):
-        """Getter for W2"""
+        """Return weights vector for output neuron"""
         return self.__W2
 
     @property
     def b2(self):
-        """Getter for b2"""
+        """Return bias for the output neuron"""
         return self.__b2
 
     @property
     def A2(self):
-        """Getter for A2"""
+        """Return activated output for the output neuron"""
         return self.__A2
 
     def forward_prop(self, X):
-        """Calculates the forward propagation of the neural network"""
+        """ Calculates the forward propagation of the neural network
 
-        Z1 = np.matmul(self.__W1, X) + self.__b1
-        self.__A1 = 1 / (1 + np.exp(-Z1))
-
-        Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
-        self.__A2 = 1 / (1 + np.exp(-Z2))
-
+        Args:
+            X (numpy.array): Input data with shape (nx, m)
+        """
+        z = np.matmul(self.__W1, X) + self.__b1
+        sigmoid = 1 / (1 + np.exp(-z))
+        self.__A1 = sigmoid
+        z = np.matmul(self.__W2, self.__A1) + self.__b2
+        sigmoid = 1 / (1 + np.exp(-z))
+        self.__A2 = sigmoid
         return self.__A1, self.__A2
