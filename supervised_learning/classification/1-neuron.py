060926036dac5bf6e72705afdeb7367cc3e359e8
Kalisa Auriane <a.kalisa@alustudent.com>
2026-09-14 00:45:08 +0200

    Add Neuron class for binary classification
    Implemented Neuron class for binary classification with initialization and properties for weights, bias, and output.

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -1,17 +1,47 @@
 #!/usr/bin/env python3
+"""Class Neuron that defines a single neuron performing binary classification
+"""
+
 
 import numpy as np
 
-Neuron = __import__('1-neuron').Neuron
 
-lib_train = np.load('../data/Binary_Train.npz')
-X_3D, Y = lib_train['X'], lib_train['Y']
-X = X_3D.reshape((X_3D.shape[0], -1)).T
+class Neuron:
+    """ Class Neuron
+    """
 
-np.random.seed(0)
-neuron = Neuron(X.shape[0])
-print(neuron.W)
-print(neuron.b)
-print(neuron.A)
-neuron.A = 10
-print(neuron.A)
+    def __init__(self, nx):
+        """ Instantiation function of the neuron
+
+        Args:
+            nx (_type_): _description_
+
+        Raises:
+            TypeError: _description_
+            ValueError: _description_
+        """
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive')
+
+        # initialize private instance attributes
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+        # getter function
+    @property
+    def W(self):
+        """Return weights"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Return bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Return output"""
+        return self.__A
