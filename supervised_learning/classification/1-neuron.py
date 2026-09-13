ad9243238b0c96894d81db26e16311245ae57ca0
Kalisa Auriane <a.kalisa@alustudent.com>
2026-09-14 00:42:23 +0200

    Refactor Neuron class and load training data
    Refactored neuron initialization and added data loading.

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -1,31 +1,17 @@
+#!/usr/bin/env python3
+
 import numpy as np
 
-class Neuron:
-    """Defines a single neuron performing binary classification with private attributes."""
-    
-    def __init__(self, nx):
-        """Initializes the private neuron."""
-        if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
-        if nx < 1:
-            raise ValueError("nx must be positive")
-        
-        # Initialize private attributes
-        self.__W = np.random.randn(1, nx)
-        self.__b = 0
-        self.__A = 0
+Neuron = __import__('1-neuron').Neuron
 
-    @property
-    def W(self):
-        """Getter for the private weights vector."""
-        return self.__W
+lib_train = np.load('../data/Binary_Train.npz')
+X_3D, Y = lib_train['X'], lib_train['Y']
+X = X_3D.reshape((X_3D.shape[0], -1)).T
 
-    @property
-    def b(self):
-        """Getter for the private bias."""
-        return self.__b
-
-    @property
-    def A(self):
-        """Getter for the private activated output."""
-        return self.__A
+np.random.seed(0)
+neuron = Neuron(X.shape[0])
+print(neuron.W)
+print(neuron.b)
+print(neuron.A)
+neuron.A = 10
+print(neuron.A)
