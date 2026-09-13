6750c20b70f4cb84d9279330ede745cc0a3129f5
Kalisa Auriane <a.kalisa@alustudent.com>
2026-09-14 00:41:36 +0200

    Refactor neuron initialization and update docstrings

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,18 +1,30 @@
+#!/usr/bin/env python3
+"""Class Neuron that defines a single neuron performing binary classification
+"""
+
+
 import numpy as np
 
+
 class Neuron:
-    """Defines a single neuron performing binary classification."""
-    
+    """ Class Neuron
+    """
+
     def __init__(self, nx):
-        """Initializes the neuron."""
+        """ Instantiation function of the neuron
+
+        Args:
+            nx (_type_): _description_
+
+        Raises:
+            TypeError: _description_
+            ValueError: _description_
+        """
         if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
+            raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError("nx must be positive")
-        
-        # Initialize weights using a standard normal distribution
-        self.W = np.random.randn(1, nx)
-        # Initialize bias to 0
+            raise ValueError('nx must be a positive integer')
+
+        self.W = np.random.normal(size=(1, nx))
         self.b = 0
-        # Initialize activated output to 0
         self.A = 0
